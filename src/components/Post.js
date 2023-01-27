0f5f1c5ad3cb88327955dd16887ebe2dd9b30c3d
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-01-27 20:04:48 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,11 @@
-import React from 'react'
-const Post = () => {
-    return (
-        <div className="post">
+import React from "react";
+const Post = ({ ele }) => {
+  return (
+    <div className="post">
+      <h3>{ele.title}</h3>
+      <p>{ele.body}</p>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export { Post }
+export { Post };

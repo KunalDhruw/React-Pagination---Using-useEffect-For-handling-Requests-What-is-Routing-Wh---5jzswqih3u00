cbc1a9f1b586e04b2156c0799bb82c7df32d015d
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-01-27 20:05:30 +0530

    Update fetchPosts.js

--- src/api/fetchPosts.js
+++ src/api/fetchPosts.js
@@ -1,5 +1,7 @@
 const fetchPosts = async (page, limit) => {
     // optionally write fetching logic here or somewhere else
-}
+    let url = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`;
+    return fetch(url);
+};
 
 export { fetchPosts }
